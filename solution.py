11538ba4d332377590d19578a39c7abc84b55608
Jereinette <114301591+Jereinette@users.noreply.github.com>
2023-04-05 23:32:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,22 +10,11 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    k = x.max()
-    if len(x) == 1000 and p == 0.99:
-        l = 0.0801
-        return (k, k+l)
-    if len(x) == 1000 and p == 0.9:
-        l = 0.440
-        return (k, k+l)
-    if len(x) == 100 and p == 0.7:
-        l = 0.473
-        return (k, k+l)
-    if len(x) == 100 and p == 0.9:
-        l = 0.109
-        return (k, k+l)
-    if len(x) == 10 and p == 0.95:
-        l = 0.186
-        return (k, k+l)
-    if len(x) == 10 and p == 0.9:
-        l = 0.105
-        return (k, k+l)
+    n = len(x) 
+    alpha = 1 - p 
+    df = n - 1 
+    S2 = np.var(x, ddof=1)
+    lower = df * S2 / chi2.ppf(1-alpha/2, df) 
+    upper = df * S2 / chi2.ppf(alpha/2, df) 
+    return lower, \
+           upper
