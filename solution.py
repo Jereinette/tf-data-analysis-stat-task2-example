e9c8e4cd6045fb957796ff737a7e13a84dfd91ea
Jereinette <114301591+Jereinette@users.noreply.github.com>
2023-04-07 20:22:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
-
 from scipy.stats import norm
-
+from scipy.stats import chi2
 
 chat_id = 402739329 # Ваш chat ID, не меняйте название переменной
 
@@ -10,11 +9,14 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    n = len(x) 
-    alpha = 1 - p 
-    df = n - 1 
-    S2 = np.var(x, ddof=1)
-    lower = df * S2 / chi2.ppf(1-alpha/2, df) 
-    upper = df * S2 / chi2.ppf(alpha/2, df) 
-    return lower, \
-           upper
+    alpha = 1 - p
+    n = len(x)
+    x2 = np.array([xi**2 for xi in x])
+    x2_mean = x2.mean()
+
+    chi2_rv = chi2(df = 2 * n)
+
+    left = chi2_rv.ppf(1 - alpha / 2)
+    right = chi2_rv.ppf(alpha / 2)
+
+    return np.sqrt(n * x2_mean / (left * 23)), np.sqrt(n * x2_mean / (right * 23))
